d888031e4d16cb0ce815afc2dec58efff0003822
alvin.abhinav <alvin.abhinav@ithands.net>
2019-06-15 21:32:36 +0530

    Updates

---
+++ precache-manifest.9a135c365c7452a4086ed45d58831c21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c03c8757b4d8ea252240aebc7faf1ca4",
+    "url": "/react-tailwindcss-profile/index.html"
+  },
+  {
+    "revision": "5535740f21d4ce703042",
+    "url": "/react-tailwindcss-profile/static/css/main.ee652055.chunk.css"
+  },
+  {
+    "revision": "7d844aa174285730e0a8",
+    "url": "/react-tailwindcss-profile/static/js/2.9d803c65.chunk.js"
+  },
+  {
+    "revision": "5535740f21d4ce703042",
+    "url": "/react-tailwindcss-profile/static/js/main.732eef75.chunk.js"
+  },
+  {
+    "revision": "4f08b78f906268633de5",
+    "url": "/react-tailwindcss-profile/static/js/runtime~main.f71b7bb9.js"
+  }
+]);
