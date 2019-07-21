94d3137a1a8a7ccbac1b9b53581ef36c384921e7
Alvin Abhinav <alvin.abhinav@ithands.net>
2019-07-21 11:35:56 +0530

    security vulnerabilities

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Main from './components/main';
 import Footer from './components/footer';
 import Header from './components/header';
-import { Link } from 'react-router-dom';
+
 class App extends Component {
   render() {
     return (
